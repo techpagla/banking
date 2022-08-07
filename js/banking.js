60eb4bd97c8e5a46eeb3b9047a8285053d6ca98d
thedarkhunterasif <iasif619@gmail.com>
2022-08-07 17:54:27 +0600

    third commit

--- js/banking.js
+++ js/banking.js
@@ -1,14 +1,29 @@
 
-//deposit function 
-document.getElementById('deposit-btn').addEventListener('click', function(){
-    var depositText = document.getElementById('deposit-value').innerText;
+var depositInputValue;
+function calculate (inputId, showId) {
+    
+    var depo = document.getElementById(inputId).value;
+     depositInputValue = parseFloat(depo);
+    var depositText = document.getElementById(showId).innerText;
     var previousDeposit = parseFloat(depositText);
-    var depo = document.getElementById('deposit').value;
-    var depositInputValue = parseFloat(depo);
     var newDeposit = previousDeposit;
     newDeposit = newDeposit + depositInputValue;
-    document.getElementById('deposit-value').innerText = newDeposit;
-    document.getElementById('deposit').value = "";
+    document.getElementById(showId).innerText = newDeposit;
+    document.getElementById(inputId).value = "";
+    return depositInputValue;
+}
+//deposit function 
+document.getElementById('deposit-btn').addEventListener('click', function(){
+    // var depositText = document.getElementById('deposit-value').innerText;
+    // var previousDeposit = parseFloat(depositText);
+    // var depo = document.getElementById('deposit').value;
+    // var depositInputValue = parseFloat(depo);
+    // var newDeposit = previousDeposit;
+    // newDeposit = newDeposit + depositInputValue;
+    // document.getElementById('deposit-value').innerText = newDeposit;
+    // document.getElementById('deposit').value = "";
+    
+    calculate('deposit', 'deposit-value');
 
     //balance add by deposit
     var balanceText = document.getElementById('balance').innerText;
@@ -18,19 +33,21 @@ document.getElementById('deposit-btn').addEventListener('click', function(){
 });
 
 document.getElementById('withdraw-btn').addEventListener('click', function(){
-    var withdrawText = document.getElementById('withdraw-value').innerText;
-    var previousWithdraw = parseFloat(withdrawText);
-    var depo = document.getElementById('withdraw').value;
-    var withdrawInputValue = parseFloat(depo);
-    var newDeposit = previousWithdraw;
-    newDeposit = newDeposit + withdrawInputValue;
-    document.getElementById('withdraw-value').innerText = newDeposit;
-    document.getElementById('withdraw').value = "";
+    // var withdrawText = document.getElementById('withdraw-value').innerText;
+    // var previousWithdraw = parseFloat(withdrawText);
+    // var depo = document.getElementById('withdraw').value;
+    // var withdrawInputValue = parseFloat(depo);
+    // var newDeposit = previousWithdraw;
+    // newDeposit = newDeposit + withdrawInputValue;
+    // document.getElementById('withdraw-value').innerText = newDeposit;
+    // document.getElementById('withdraw').value = "";
+
+    calculate('withdraw', 'withdraw-value');
 
     //balance add by withdraw
     var balanceText = document.getElementById('balance').innerText;
     var previousBalance = parseFloat(balanceText);
-    var newBalance =  previousBalance - withdrawInputValue;
+    var newBalance =  previousBalance - depositInputValue;
     document.getElementById('balance').innerText = newBalance;
 });
 
